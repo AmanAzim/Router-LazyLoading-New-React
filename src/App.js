872f6244a2e381aba61a4d227d5add4c17cfc25b
Aman <rumman.djuice1@gmail.com>
2019-07-17 19:30:15 +0200

    "Routing Prompt added"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Suspense } from 'react';
-import { BrowserRouter, Route, NavLink, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, NavLink, Redirect, Prompt } from 'react-router-dom';
 
 import User from './containers/User';
 import Welcome from './containers/Welcome';
@@ -10,7 +10,7 @@ const Posts=React.lazy(()=>import('./containers/Posts'));
 class App extends Component {
   state={
     show:false,
-    authenticated:true
+    authenticated:false
   }
 
   modeHandler=()=>{
@@ -33,11 +33,17 @@ class App extends Component {
 
           {/*<Route path="/user" component={User} exact />*/}
           <Route path="/user" render={()=>{ return this.state.authenticated? <User />:  <Redirect to="/"/> }} />
+          <Prompt when={!this.state.authenticated}
+                  message={(location)=>{return location.pathname.startsWith('/user')? 'Are you logedin ?':true}}
+          />
+          <button onClick={()=>this.setState({authenticated:!this.state.authenticated})}>Login</button>
+
 
           <Route path="/posts" render={()=><Suspense fallback={<div>Loading..</div>}><Posts /></Suspense>} />
 
           <button onClick={this.modeHandler}>Show Posts</button>
           {this.state.show? <Suspense fallback={<div>Loading..</div>}><Posts /></Suspense>:null}
+
         </React.Fragment>
       </BrowserRouter>
     );
